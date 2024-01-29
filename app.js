9c0cf221645162c4edf73f21f984da97c730a01e
Srujann31 <155942978+Srujann31@users.noreply.github.com>
2024-01-29 11:49:39 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -25,12 +25,6 @@ window.addEventListener('scroll', () => {
         element.style.opacity = scroll / (sectionY.top + section_height);
     })
 
-    big_title.style.opacity = - scroll / (header_height / 2) + 1;
-    shadow.style.height = `${scroll * 0.5 + 300}px`;
-
-    
-    p.style.opacity = - scroll / (header_height / 2) + 1;
-    shadow.style.height = `${scroll * 0.5 + 300}px`;
 
     content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`;
     image_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -50 + 50}px)`;
